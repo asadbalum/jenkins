e40a4a19a616eed636e747112e81d10aa1004a78
Asad Blum <asadblum@gmail.com>
2021-07-08 23:53:06 +0300

    sample

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__)
 
 class user:
     def __init__(self, name, email, age) -> None:
@@ -10,3 +13,12 @@ class user:
 
     def tenY(self):
         return self.age + 10
+
+
+@app.route('/')
+def home():
+    agent = user('Asad','',25)
+    return '<b>Hello '+ agent.name + '</b>'
+
+if __name__=='__main__':
+    app.run(debug=True)
